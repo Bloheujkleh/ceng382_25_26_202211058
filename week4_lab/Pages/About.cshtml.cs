d56a731f9b13dc164388a683d753da40d4f21f17
Bloheujkleh <63818419+Bloheujkleh@users.noreply.github.com>
2025-03-18 09:52:14 +0000

    week4 pushed

---
+++ week4_lab/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBootstrapApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
